2b0e51c867da11683fbcf0ceb9915f6f792d390f
mraraujo <marquinhoaraujo@gmail.com>
2022-09-12 21:36:44 -0300

    test: setup for testing with specific package for component tests

--- apps/demo/build.gradle.kts
+++ apps/demo/build.gradle.kts
@@ -1,25 +1,39 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import org.gradle.api.tasks.testing.logging.TestLogEvent
 
 plugins {
 	id("org.springframework.boot") version "2.7.3"
 	id("io.spring.dependency-management") version "1.0.13.RELEASE"
 	kotlin("jvm") version "1.6.21"
 	kotlin("plugin.spring") version "1.6.21"
-
 }
 
 group = "org.quality"
-version = "0.0.1"
+version = "1.0"
 java.sourceCompatibility = JavaVersion.VERSION_17
 
 repositories {
 	mavenCentral()
 }
 
+sourceSets {
+	create("componentTest") {
+		kotlin {
+			compileClasspath += main.get().output + configurations.testRuntimeClasspath
+			runtimeClasspath += main.get().output + compileClasspath
+		}
+		resources {
+			srcDirs("/src/componentTest/resources")
+		}
+	}
+}
+
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
-	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.7.10")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+
 
 	// Test
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
@@ -36,4 +50,26 @@ tasks.withType<KotlinCompile> {
 
 tasks.withType<Test> {
 	useJUnitPlatform()
+
+	testLogging {
+		events = setOf(
+			TestLogEvent.STARTED,
+			TestLogEvent.SKIPPED,
+			TestLogEvent.FAILED,
+			TestLogEvent.PASSED
+		)
+		showCauses = true
+		showStackTraces = true
+	}
+}
+
+tasks.register<Test>("unitTest") {
+	testClassesDirs = sourceSets["test"].output.classesDirs
+	classpath = sourceSets["test"].runtimeClasspath
+
+}
+
+tasks.register<Test>("componentTest") {
+	testClassesDirs = sourceSets["componentTest"].output.classesDirs
+	classpath = sourceSets["componentTest"].runtimeClasspath
 }
